439f9fa712462ba11f171ccf5f9befce49d9403e
sylvia31096 <sylviachach@gmail.com>
2021-04-27 21:03:24 +0300

    Added Day 2 Challenge

---
+++ arithmetic.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+	"math"
+)
+
+// Complete the solve function below.
+func solve(meal_cost float64, tip_percent int32, tax_percent int32) {
+	
+
+	tip := float64(tip_percent)/100*meal_cost
+	tax := float64(tax_percent)/100*meal_cost
+
+	total_cost := meal_cost + tip + tax
+	rounded_total_cost := int(math.Round(total_cost))
+
+	fmt.Printf("%d",rounded_total_cost)
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+    meal_cost, err := strconv.ParseFloat(readLine(reader), 64)
+    checkError(err)
+
+    tip_percentTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+    checkError(err)
+    tip_percent := int32(tip_percentTemp)
+
+    tax_percentTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+    checkError(err)
+    tax_percent := int32(tax_percentTemp)
+
+    solve(meal_cost, tip_percent, tax_percent)
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
